e533471f36f2fb4831926b70bcb1d6d3e54862e5
hashaem <hashemalsaie0457@gmail.com>
2025-05-10 19:12:53 +0300

    some adds

--- JS/authentication.js
+++ JS/authentication.js
@@ -1,6 +1,6 @@
 
 
-async function login() {
+async function Signin() {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
     // const loginBtn = document.getElementById('login').value;
@@ -17,13 +17,33 @@ async function login() {
         });
 
         if(!response.ok){
-            throw new Error('Invalid credentials');
+            throw new Error('Username or password is uncorrect');
         }
         const jwt = await response.json();
-        console.log(jwt);
+        localStorage.setItem('session', jwt)
+        document.getElementById('Signin').style.display = 'none';
+        document.getElementById('Profile').style.display = 'block';
+        UserProfile()
         
 
     }catch(error) {
-
+        document.getElementById('error-message').style.display = 'block';
+        document.getElementById('error-message').innerText = error.message;
     }
+}
+
+function SignOut(){
+    localStorage.removeItem('session');
+}
+
+async function fetchGraphql(query) {
+    const response = await fetch('https://learn.reboot01.com/api/graphql-engine/v1/graphql',{
+        method: 'POST',
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem('session')}`,
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({query}),
+    });
+    return response.json();
 }

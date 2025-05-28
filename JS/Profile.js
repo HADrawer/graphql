76afb4b7d1dc9a4fdc4e6d745279c63fbf5971b8
Hashem <hashemalsaie0457@gmail.com>
2025-05-28 23:51:36 +0300

    some graph

--- JS/Profile.js
+++ JS/Profile.js
@@ -2,24 +2,26 @@ async function UserProfile() {
     
     const graphQuery = `
     query {
-        {
+        
     user {
         id
         firstName
         lastName
         login
         email
-    }
+    
     }
     }
     `;
 
-    // try{
-    //     const data = await fetchGraphql(graphQuery);
-    //     const user = data.data.user[0];
+    try{
+        const data = await fetchGraphql(graphQuery);
+        const user = data.data.user[0];
 
 
-    //     document.getElementById()
-    // }
+        document.getElementById('username_title').textContent = `${user.login}`;
+    } catch(error){
+        console.error('Error fetching data:', error);
+    }
 
 }
